e14748a178c761328d44af91325916f9f9490b0b
henri.chevallier <henri.chevallier@gmail.com>
2018-03-16 06:29:43 -0400

    Recipe picture controller modif

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
   
 before_action :set_recipe, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
 
 def index
     @recipes = current_user.recipes
@@ -14,7 +15,15 @@ end
 def create
   @recipe = current_user.recipes.build(recipe_params)
   if @recipe.save
-    redirect_to @recipe, notice:"Recipe published"
+    if params[:images]
+      params[:images].each do |i|
+        @recipe.pictures.create(image: i)
+      end
+    end
+    
+    @pictures = @recipe.pictures
+        
+    redirect_to edit_recipe_path(@recipe), notice:"Recipe published"
   else
     render :new
   end
@@ -23,14 +32,24 @@ end
 
   
 def show
+  @pictures = @recipe.pictures
 end
   
 def edit
+  @pictures = @recipe.pictures
 end
 
 def update
   if @recipe.update(recipe_params)
-    redirect_to @recipe, notice:"Recipe updated"
+    if @recipe.save
+    if params[:images]
+      params[:images].each do |i|
+        @recipe.pictures.create(image: i)
+      end
+    end
+    
+    @pictures = @recipe.pictures
+    redirect_to edit_recipe_path(@recipe), notice:"Recipe updated"
   else
     render :edit
   end
@@ -45,6 +64,13 @@ end
   
 def recipe_params
   params.require(:recipe).permit(:recipe_name, :recipe_external_link, :recipe_category, :recipe_description)
+end
+
+def require_same_user
+  if current_user.id != @recipe.user_id
+    flash[:danger] = "You can't edit this recipe"
+    redirect_to root_path
+  end
 end
   
   
