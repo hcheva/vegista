3064274e3961a4ad1a95afc7948adf67bf93acc9
henri.chevallier <henri.chevallier@gmail.com>
2018-03-17 09:01:38 -0400

    heroku test

--- db/migrate/20180316100126_create_pictures.rb
+++
@@ -1,9 +0,0 @@
-class CreatePictures < ActiveRecord::Migration
-  def change
-    create_table :pictures do |t|
-      t.references :recipe, index: true
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,51 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180316100221) do
-
-  create_table "pictures", force: true do |t|
-    t.integer  "recipe_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
-  add_index "pictures", ["recipe_id"], name: "index_pictures_on_recipe_id"
-
-  create_table "recipes", force: true do |t|
-    t.string   "recipe_name"
-    t.string   "recipe_external_link"
-    t.string   "recipe_category"
-    t.text     "recipe_description"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "nickname"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-  end
-
+  
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
