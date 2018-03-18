1e2fdf3271a3bfd4093fbdab4196304b6bc44251
henri.chevallier <henri.chevallier@gmail.com>
2018-03-18 12:17:32 -0400

    Photos view added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,49 +2,73 @@ class GamesController < ApplicationController
   
 before_action :set_game, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
-  
-  
-  
-  def index
+before_action :require_same_user, only: [:edit, :update]
+
+def index
     @game = current_user.games
-  end
+end
   
-  def new
-    @game = current_user.games.build
-  end
+def new
+  @game = current_user.games.build
+end
   
-  def create
-    @game = current_user.games.build(game_params)
-    if @game.save
-      redirect_to @game, notice:"Game posted!"
-    else
-      render :new
+def create
+  @game = current_user.games.build(game_params)
+  if @game.save
+    if params[:images]
+      params[:images].each do |i|
+        @game.photos.create(image: i)
+      end
     end
-  end
+    @photos = @game.photos
+  redirect_to edit_game_path(@game), notice:"Game published"
+  else
+    render :new
+  end 
+end
+
   
-  def show
-  end
+def show
+  @photos = @game.photos
+end
   
-  def edit
-  end
-  
-  def update
-    if @game.update(game_params)
-      redirect_to @game, notice:"Game updated!"
-    else
-      render :edit
+def edit
+  @photos = @game.photos
+end
+
+def update
+  if @game.update(game_params)
+    if params[:images]
+      params[:images].each do |i|
+        @game.photos.create(image: i)
+      end
     end
+    @photos = @game.photos
+    
+    redirect_to edit_game_path(@game), notice: "Game updated"
+  else
+    render :edit
   end
+end
+  
   
 private
-  def set_game
-    @games = Game.find(params[:id])
+  
+def set_game
+  @game = Game.find(params[:id])
+end
+  
+def game_params
+  params.require(:game).permit(:game_name, :game_external_link, :game_genre, :game_platform, :game_description)
+end
+  
+def require_same_user
+  if current_user.id != @game.user_id
+    flash[:danger] = "You can't edit this game"
+    redirect_to root_path
   end
+end
+
   
-  def game_params
-    params.require(:game).permit(:game_name, :game_description, :game_external_link, :game_genre, :game_platform)
-  end
-  
-  
-  
+
 end
