e4d40a9b0768d180ceae6c762d7f04b86dcf1997
henri.chevallier <henri.chevallier@gmail.com>
2018-03-18 10:24:31 -0400

    Game controller added

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,50 @@
+class GamesController < ApplicationController
+  
+before_action :set_game, only: [:show, :edit, :update]
+before_action :authenticate_user!, except: [:show]
+  
+  
+  
+  def index
+    @games = current_user.games
+  end
+  
+  def new
+    @games = current_user.games.build
+  end
+  
+  def create
+    @games = current_user.games.build(game_params)
+    if @game.save
+      redirect_to @game, notice:"Game posted!"
+    else
+      render :new
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @game.update(game_params)
+      redirect_to @game, notice:"Game updated!"
+    else
+      render :edit
+    end
+  end
+  
+private
+  def set_game
+    @games = Game.find(params[:id])
+  end
+  
+  def game_params
+    params.require(:game).permit(:game_name, :game_description, :game_external_link, :game_genre, :game_platform)
+  end
+  
+  
+  
+end
