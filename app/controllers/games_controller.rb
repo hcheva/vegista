e497fb0ba8a11978dc42032181fa60179642d2e8
henri.chevallier <henri.chevallier@gmail.com>
2018-03-21 12:40:16 -0400

    Group games by day / home

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,7 @@ before_action :set_game, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
 before_action :require_same_user, only: [:edit, :update]
 
+ 
 def index
     @game = current_user.games
     @games = Game.all.order("created_at DESC")
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
-    @games = Game.all.order("created_at DESC")
+    @games = Game.all
+    @game_days = Game.all.group_by { |g| g.created_at.strftime("%B, %d") }
   end
+  
   
   def show
     render template: "pages/#{params[:page]}"
