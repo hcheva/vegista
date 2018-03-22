fe400b22b8171d9a10dea5864bd47f69ea975975
henri.chevallier <henri.chevallier@gmail.com>
2018-03-22 11:08:05 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
   end
+  
+  # TEST TEST TEST
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
